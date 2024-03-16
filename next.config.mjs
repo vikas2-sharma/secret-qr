544a79fa9feefbf85fc94122acc25d9180746cf1
Vikas Sharma <vikassharma333666@gmail.com>
2024-03-16 21:40:39 +0530

    build fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      fallback: {
+        fs: false,
+      },
+    };
+  },
+};
 
 export default nextConfig;
