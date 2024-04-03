cd6778e2cbf14f59ebbcee41ffbabd8ed0de084c
Vikas Sharma <vikassharma333666@gmail.com>
2024-04-03 22:43:31 +0530

    replaced home layout.tsx content into page.tsx

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -3,18 +3,7 @@ import Header from "../components/header";
 import HomeContent from "../components/HomeContent";
 
 function Layout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-0">
-        <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-          <div className="w-full h-full absolute top-16 sm:p-8 p-4">
-            <HomeContent />
-          </div>
-        </div>
-      </main>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
 
 export default Layout;
